358c029f8678209bcdbf5476d22c3dca92823ab8
calorie142 <23e1811e@shinshu-u.ac.jp>
2024-11-28 18:05:55 +0900

    2nd

--- joho.py
+++ joho.py
@@ -19,32 +19,31 @@ for i in range(100):
     late = (100 - i) / 10000
     time.sleep(late)
 
-if i == 99:
-    text = ""
-    text = st.text_input("あなたの名前を教えてください")
-    if text != "":
-        st.write("あなたの名前は" +text+ "です")
+text = ""
+text = st.text_input("あなたの名前を教えてください")
+if text != "":
+    st.write("あなたの名前は" +text+ "です")
 
-    condition = st.slider("あなたの今日の調子は？",0,10,5)
-    st.write("コンディション" ,condition)
+condition = st.slider("あなたの今日の調子は？",0,10,5)
+st.write("コンディション" ,condition)
 
-    option = st.selectbox("好きな数字を教えてください" ,list(["1番","2番","3番","4番"]))
-    st.write("あなたが選択したのは",option,"です")
+option = st.selectbox("好きな数字を教えてください" ,list(["1番","2番","3番","4番"]))
+st.write("あなたが選択したのは",option,"です")
 
 
-    left_column, right_column = st.columns(2)
-    button = left_column.button('右カラムに文字を表示')
-    if button:
-        right_column.write('ここは右カラムです')
+left_column, right_column = st.columns(2)
+button = left_column.button('右カラムに文字を表示')
+if button:
+    right_column.write('ここは右カラムです')
 
-    from PIL import Image #PILをpip install pillowを実施する
-    img = Image.open("増田 來亜 _ Masuda Kureaのストーリーズが消える前にInstagramで見よう。 - Google Chrome 2024_11_21 18_30_53.png")
+from PIL import Image #PILをpip install pillowを実施する
+img = Image.open("増田 來亜 _ Masuda Kureaのストーリーズが消える前にInstagramで見よう。 - Google Chrome 2024_11_21 18_30_53.png")
     #自分の画像のファイル名にする(room.jpgは例えば)
     #自分のPCの画像を同じフォルダに入れて指定する
-    st.image(img, caption='増田來亜', use_container_width=True)
+st.image(img, caption='増田來亜', use_container_width=True)
 
-    df = pd.DataFrame(np.random.rand(100,2)/[50,50] + [36.64,138.19],columns = ['lat','lon',])
+df = pd.DataFrame(np.random.rand(100,2)/[50,50] + [36.64,138.19],columns = ['lat','lon',])
     #lat lon 緯度と経度
     #緯度と経度から地図に書き込む
-    st.map(df)
-    st.table(df)
+st.map(df)
+st.table(df)
